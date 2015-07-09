e3982b7391c7cbe5af20de123e02a893d6e333bd
Stas <leonsted9@gmail.com>
2015-07-09 16:11:46 +0300

    Four Problem

--- 04.java
+++ 04.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		int pal;
+		int max = 0;
+		int maxP;
 		for(int znach1 = 100;znach1<1000;znach1++){
 			for(int znach2 = 100; znach2<1000; znach2++){
 				pal = znach1*znach2;
 				String sPal = Integer.toString(pal);
 				String str = new StringBuffer(sPal).reverse().toString();
 				if(sPal.equals(str)){
-					System.out.println("Число-палиндром: " + sPal);
-				}
+					maxP = Integer.parseInt(str);
+					if(max<maxP){
+						max =maxP;
+					}else System.out.println("Наибольший палиндром: "+max);
+				}	
 			}
+
 		}
 	}
 }
